bdd69617b1e8be1994f93c06653b30e872e9f252
Kidren251 <taane.renaud@gmail.com>
2024-02-08 19:40:27 +0100

    version6

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ const navlink = document.querySelector(".nav-link");
 MenuHamburger.addEventListener("click", () => {
   navlink.classList.toggle("mobile-menu");
 });
+
+/**NAV **/
+let position = 0;
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY < position) {
+    navbar.style.top = 0;
+  } else {
+    navbar.style.top = "-180px";
+  }
+  position = window.scrollY;
+});
